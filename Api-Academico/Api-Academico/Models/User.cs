ed1058c76f4bc43c975429ed9ae059d1d0a693c7
EdwardSPO <sp410110@gmail.com>
2022-08-28 14:23:16 -0500

    init

---
+++ Api-Academico/Api-Academico/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+namespace Api_Academico.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        [ForeignKey("Rol")]
+        public int IdRol { get; set; }
+        public virtual Roles Rol { get; set; }
+    }
+}
